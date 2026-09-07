0a8a6b0220653fb1488b270ff496e10e7ad9c8fc
kanchanr2208 <267316562+kanchanr2208@users.noreply.github.com>
2026-09-07 04:18:50 +0000

    Add homepage component, add mainContent in index.js - Create homepage component in src with CSS - Download backgrounds for homepage component - Create mainContent div in index.js for switching mainContent - Currently add homepageContent in mainContent

---
+++ src/components/homepageContent.js
@@ -0,0 +1,22 @@
+export function loadHomepageContent() {
+    const homepageContainer = document.createElement("div")
+    const homepageColumnOne = document.createElement("div")
+    const homepageColumnTwo = document.createElement("div")
+    const homepageFindUs = document.createElement("div")
+    const homepageAboutUs = document.createElement("div")
+
+    homepageContainer.classList.add("homepage-container")
+    homepageColumnOne.classList.add("homepage-column-one", "homepage-menu")
+    homepageColumnTwo.classList.add("homepage-column-two")
+    homepageFindUs.classList.add("homepage-find-us")
+    homepageAboutUs.classList.add("homepage-about-us")
+
+    homepageColumnOne.textContent = "Menu"
+    homepageFindUs.textContent = "Find Us"
+    homepageAboutUs.textContent = "About Us"
+
+    homepageColumnTwo.append(homepageFindUs, homepageAboutUs)
+    homepageContainer.append(homepageColumnOne, homepageColumnTwo)
+
+    return homepageContainer
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 import "./styles.css";
 import { loadHeader } from "./components/header.js";
 import { loadFooter } from "./components/footer.js";
+import { loadHomepageContent } from "./components/homepageContent.js";
 
 loadHeader();
+
+const mainContent = document.createElement("div")
+mainContent.classList.add("main-content")
+mainContent.id = "main-content"
+document.body.append(mainContent)
+
+mainContent.append(loadHomepageContent())
+
 loadFooter();
